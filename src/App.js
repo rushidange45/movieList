07d87dd4484c860d469cb3c07ef6d663aeccb774
rushidange45 <rushikeshd80@gmail.com>
2021-10-11 14:50:29 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import './App.css';
+import Card from './Components/Card';
+import Data from './Components/Data';
+
+
+function App() {
+  return (
+
+    <>
+      {
+        Data.map((val) => {
+          return (
+
+            <Card
+              title={val.title}
+              collection={val.collection}
+              Distributor={val.Distributor}
+              imgsrc={val.imgsrc} />
+          );
+        })
+      }
+    </>
+
+
+  );
+
+}
+
+export default App;
